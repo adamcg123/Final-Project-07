c479483e1e28b07c7687643b844a40212327e61f
Rick Johnson <carl4917@umn.edu>
2022-03-16 19:35:08 -0500

    adding server inmfo except mongoose

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const compression = require("compression");
+
+const PORT = process.env.PORT || 3001;
+// const MONGODB_URI = process.env.MONGODB_URI || "";
+
+const app = express();
+
+
+app.use(compression());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+
+app.use(require("./routes/api"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
