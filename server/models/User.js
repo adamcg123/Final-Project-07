fc01854bbeb1fd33c712d5cb54c207b3a069d4f8
unknown <Sophia Nguyen>
2022-03-16 20:09:03 -0500

    required mongoose connection and changed into a UserSchema for the user model

--- server/models/User.js
+++ server/models/User.js
@@ -1,2 +1,53 @@
-const {Model, DataTypes} = require("sequelize");
-const sequelize =
+const { Schema, model} = require("mongoose");
+const mongoose = require("../config/connection");
+const bycrpt = require("bycrpt");
+
+// check for password
+class User extends model {
+    checkPassword(loginPw) {
+        return bycrpt.compareSync(loginPw, this.password);
+    }
+}
+
+const UserSchema = new Schema(
+    {
+        id: {
+            type: String,
+            required: true,
+            trimmed: true,
+            maxlength: 28
+        },
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trimmed: true,
+            maxlength: 28
+        },
+        email: {
+            type: String,
+            required: true,
+            trimmed: true,
+            unique: true,
+            match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/]
+        },
+        password: {
+            type: String,
+            required: true,
+            validate: {
+                len:[12]
+            }
+        },
+        followers: [{
+            type: Schema.Types.ObjectId, 
+            ref: "Users"
+        }],
+        following: [{
+            type: Schema.Types.ObjectId,
+            ref: "Users"
+        }]
+    }
+)
+
+const User = model("User", UserSchema);
+module.exports = User;
