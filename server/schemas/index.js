3081796094a215fd5cff5d7956b5edb445966469
Rick Johnson <carl4917@umn.edu>
2022-03-16 20:36:36 -0500

    adding typedefs and resolvers, and index for both

---
+++ server/schemas/index.js
@@ -0,0 +1,6 @@
+const typeDefs = require('./typeDefs');
+const resolvers = require('./Resolvers');
+
+
+
+module.exports = { typeDefs, resolvers };
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,24 @@
+const { gql } = require('apollo-server-express');
+
+
+const typeDefs = gql`
+  type User {
+  }
+
+  type Post {
+  }
+
+  type Auth {
+
+  }
+
+  type Query {
+
+  }
+
+  type Mutation {
+  }
+`;
+
+
+module.exports = typeDefs;
