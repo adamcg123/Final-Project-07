fd19e2cfa19f969ec51db924f5d18737af86995c
zeeshan <mycomputer@Zeeshans-MacBook-Pro.local>
2022-03-20 07:55:47 -0500

    create signup page and add some links

--- client/src/components/Signin.js
+++ client/src/components/Signin.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Signin = () =>{
     return(
@@ -8,7 +9,9 @@ const Signin = () =>{
           <input type= "text"  placeholder='email'/>
               <input type= "text" placeholder='password'/>
   <button className="btn waves-effect waves-light #64b5f6 blue lighten-2">Login </button>
-
+         <h5>
+            <Link to= "/signup"> Don't have an account ? </Link>
+        </h5>
 
 
       </div>
--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
-const Signup = () =>{
+const Signin = () =>{
     return(
-        <h1> Signup </h1>
+     <div className='mycard'>
+          <div className="card auth-card input-field ">
+          <h2> Camera Cast</h2>
+          <input type= "text"  placeholder='name'/>
+          <input type= "text"  placeholder='email'/>
+              <input type= "text" placeholder='password'/>
+  <button className="btn waves-effect waves-light #64b5f6 blue lighten-2">Singup </button>
+        <h5>
+            <Link to= "/signin"> Already have an account ? </Link>
+        </h5>
+
+
+      </div>
+     </div>
     )
 }
-export default Signup;
+export default Signin;
