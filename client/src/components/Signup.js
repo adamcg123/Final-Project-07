d9e7d09b0022ee600a7ee23583cd190dfb8b454a
zeeshan <mycomputer@Zeeshans-MacBook-Pro.local>
2022-03-20 11:56:32 -0500

    Fetching data

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,15 +1,36 @@
-import React from 'react'
+import React,{useState} from 'react'
 import {Link} from 'react-router-dom'
 
+
 const Signin = () =>{
+    const [name,setName]= useState("")
+    const [password,setPassword]= useState("")
+    const [email,setEmail]= useState("")
+    const PostData = () =>{
+        fetch("/signup",{
+            method:"post",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify({
+                name:"" ,
+                password: "",
+                email: ""
+            })
+          })  .then(res=>res.json())
+            .then(data=>{
+                console.log(data);
+            })
+
+    }
     return(
      <div className='mycard'>
           <div className="card auth-card input-field ">
           <h2> Camera Cast</h2>
-          <input type= "text"  placeholder='name'/>
-          <input type= "text"  placeholder='email'/>
-              <input type= "text" placeholder='password'/>
-  <button className="btn waves-effect waves-light #64b5f6 blue lighten-2">Singup </button>
+          <input type= "text"  placeholder='name' value={name} onChange={(e)=>setName(e.target.value)}/>
+          <input type= "text"  placeholder='email' value={email} onChange={(e)=>setEmail(e.target.value)}/>
+              <input type= "text" placeholder='password' value={password} onChange={(e)=>setPassword(e.target.value)}/>
+  <button className="btn waves-effect waves-light #64b5f6 blue darken-1" onClick={()=>PostData()}>Singup </button>
         <h5>
             <Link to= "/signin"> Already have an account ? </Link>
         </h5>
