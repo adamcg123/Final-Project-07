7fa395e4a9570f1dd9c0c739f39cad5357d42bf3
adamcg123 <adam.Gondorchin26@gmail.com>
2022-03-23 18:26:06 -0500

    Updates

---
+++ precache-manifest.0bc986d69d87d051b9c462d180f39112.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27a0fec8e17179ac0b175dc28c32d0f7",
+    "url": "/Final-project-07/index.html"
+  },
+  {
+    "revision": "6c090caf3cb64623c026",
+    "url": "/Final-project-07/static/css/main.a4135bf3.chunk.css"
+  },
+  {
+    "revision": "70c9184443c804ee3210",
+    "url": "/Final-project-07/static/js/2.05df021f.chunk.js"
+  },
+  {
+    "revision": "f3df11ca4cc8eaf0f270098bfa135497",
+    "url": "/Final-project-07/static/js/2.05df021f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c090caf3cb64623c026",
+    "url": "/Final-project-07/static/js/main.c27d21c0.chunk.js"
+  },
+  {
+    "revision": "6f0af2a5229314d61647",
+    "url": "/Final-project-07/static/js/runtime-main.5235b929.js"
+  }
+]);
