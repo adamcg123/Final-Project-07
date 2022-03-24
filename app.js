b6dd73ce53e8a18165a2955b71653f115c7732df
Rick Johnson <carl4917@umn.edu>
2022-03-23 21:33:44 -0500

    testing

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const mongoose  = require('mongoose')
 const PORT = process.env.PORT || 5000
-const {MONGOURI} = require('./config/keys')
+const {MONGOURI} = require('./server/config/keys')
 
 
 mongoose.connect(MONGOURI,{
@@ -17,13 +17,13 @@ mongoose.connection.on('error',(err)=>{
     console.log("err connecting",err)
 })
 
-require('./models/user')
-require('./models/post')
+require('./server/models/user')
+require('./server/models/post')
 
 app.use(express.json())
-app.use(require('./routes/auth'))
-app.use(require('./routes/post'))
-app.use(require('./routes/user'))
+app.use(require('./server/routes/auth'))
+app.use(require('./server/routes/post'))
+app.use(require('./server/routes/user'))
 
 
 if(process.env.NODE_ENV=="production"){
