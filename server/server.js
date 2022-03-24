11366499d1df9ce69db35829a32940bab7c155da
Rick Johnson <carl4917@umn.edu>
2022-03-23 23:36:17 -0500

    more server.js edits and cleaning up file paths

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 const express = require('express')
-const app = express()
 const mongoose  = require('mongoose')
-const PORT = process.env.PORT || 5000
+const app = express()
+
+const PORT = process.env.PORT || 3001
 const {MONGOURI} = require('./config/keys')
 
 
@@ -17,24 +18,24 @@ mongoose.connection.on('error',(err)=>{
     console.log("err connecting",err)
 })
 
-require('./server/models/user')
-require('./server/models/post')
+require('./models/user')
+require('./models/post')
 
 app.use(express.json())
-app.use(require('./server/routes/auth'))
-app.use(require('./server/routes/post'))
-app.use(require('./server/routes/user'))
+app.use(require('./routes/auth'))
+app.use(require('./routes/post'))
+app.use(require('./routes/user'))
 
 
 if(process.env.NODE_ENV=="production"){
     app.use(express.static('client/public'))
     const path = require('path')
     app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'client','public','index.html'))
+        res.sendFile(path.resolve(__dirname,'../client/public/index.html'))
     })
 }
 
 app.listen(PORT,()=>{
-    console.log("server is running on",PORT)
+    console.log("server is running on port ${PORT}!")
 })
 
