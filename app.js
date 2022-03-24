f48543be6d68d17a69882913b537de6941c5fe43
zeeshan <mycomputer@Zeeshans-MacBook-Pro.local>
2022-03-23 23:35:42 -0500

    heroku link

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const app = express()
+const mongoose  = require('mongoose')
+const PORT = process.env.PORT || 5000
+const {MONGOURI} = require('./server/config/keys')
+
+
+mongoose.connect(MONGOURI,{
+    useNewUrlParser:true,
+    useUnifiedTopology: true
+
+})
+mongoose.connection.on('connected',()=>{
+    console.log("conneted to mongo yeahh")
+})
+mongoose.connection.on('error',(err)=>{
+    console.log("err connecting",err)
+})
+
+require('./server/models/user')
+require('./server/models/post')
+
+app.use(express.json())
+app.use(require('./server/routes/auth'))
+app.use(require('./server/routes/post'))
+app.use(require('./server/routes/user'))
+
+
+if(process.env.NODE_ENV=="production"){
+    app.use(express.static('client/public'))
+    const path = require('path')
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','public','index.html'))
+    })
+}
+
+app.listen(PORT,()=>{
+    console.log("server is running on",PORT)
+})
+
